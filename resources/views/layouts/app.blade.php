6dd63691ff19b9e522d0773bb9aa48acb1b8b09a
Joseph Bassey <Joemires20@yahoo.com>
2022-05-26 19:06:13 +0100

    fixed my layout slot and navigation indication with Laravel EasyNav package

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,9 @@
 </head>
 
 <body id="body-pd">
-    <header class="header position-relative d-block">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
-            <div class="container-fluid d-flex justify-content-between w-100">
+    <header class="header position-relative d-block bg-transparent border-bottom">
+        <nav class="navbar navbar-expand-lg navbar-light w-100">
+            <div class="container d-flex justify-content-between w-100">
                 <div class="d-flex align-items-center gap-2 w-100">
                     <h5 class="m-0 me-4">Users</h5>
                     <div class="form-group m-0 position-relative">
@@ -87,54 +87,41 @@
     <div class="l-navbar d-none d-md-flex p-0" id="nav-bar">
         <nav class="nav">
             <div>
-                {{-- <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a> --}}
-                <div class="nav_list"> <a href="#" class="nav_link"> <i class='bx bx-search nav_icon'></i>
-                        <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#"
-                        class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span
-                            class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a
-                        href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span
-                            class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i
-                            class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
+                <div class="nav_list">
+                    <a href="#" class="nav_link"> <i class='bx bx-search nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a>
+                    <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
                 </div>
             </div>
             <div>
-                <a href="#" class="nav_link"> <i class='bx bx-cog nav_icon'></i> <span
-                        class="nav_name">Settings</span> </a>
+                <a href="#" class="nav_link"> <i class='bx bx-cog nav_icon'></i> <span class="nav_name">Settings</span> </a>
                 {{-- <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div> --}}
                 <a href="#" class="nav_link collapse-toggler"><i class='bx bx-menu nav_icon nav-toggler'></i> </a>
             </div>
         </nav>
         <nav class="bg-white" style="flex-grow: 1; width: 40%">
-            <a href="#" class="nav_link"> <i class='bx bxs-dashboard nav_icon'></i> <span
-                    class="nav_name">Dashboard</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-group nav_icon'></i> <span
-                    class="nav_name">Users</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-collection nav_icon'></i> <span
-                    class="nav_name">Departments</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bxs-user-account nav_icon'></i> <span
-                    class="nav_name">Employee</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span
-                    class="nav_name">Activities</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span
-                    class="nav_name">Holidays</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span
-                    class="nav_name">Events</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span
-                    class="nav_name">Payroll</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bxs-dashboard nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
+            <a href="#" class="nav_link {{ Nav::isRoute('backend.users.index') }}"> <i class='bx bx-group nav_icon'></i> <span class="nav_name">Users</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-collection nav_icon'></i> <span class="nav_name">Departments</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bxs-user-account nav_icon'></i> <span class="nav_name">Employee</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span class="nav_name">Activities</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span class="nav_name">Holidays</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span class="nav_name">Events</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span class="nav_name">Payroll</span> </a>
 
-            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span
-                    class="nav_name">Accounts</span> </a>
-            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span
-                    class="nav_name">Report</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span class="nav_name">Accounts</span> </a>
+            <a href="#" class="nav_link"> <i class='bx bx-chip nav_icon'></i> <span class="nav_name">Report</span> </a>
         </nav>
         {{-- <nav class="bg-dark">
             Hello
         </nav> --}}
     </div>
     <!--Container Main start-->
-    <main class="height-100">
+    <main>
+        <div class="container py-4">
+            {{ $slot }}
+        </div>
         {{-- <h4>Main Components</h4> --}}
     </main>
     <!--Container Main end-->
@@ -164,15 +151,15 @@
             });
 
             /*===== LINK ACTIVE =====*/
-            const linkColor = document.querySelectorAll('.nav_link:not(.collapse-toggler)')
+            // const linkColor = document.querySelectorAll('.nav_link:not(.collapse-toggler)')
 
-            function colorLink() {
-                if (linkColor) {
-                    linkColor.forEach(l => l.classList.remove('active'))
-                    this.classList.add('active')
-                }
-            }
-            linkColor.forEach(l => l.addEventListener('click', colorLink))
+            // function colorLink() {
+            //     if (linkColor) {
+            //         linkColor.forEach(l => l.classList.remove('active'))
+            //         this.classList.add('active')
+            //     }
+            // }
+            // linkColor.forEach(l => l.addEventListener('click', colorLink))
 
             // Your code to run since DOM is loaded and ready
         })
